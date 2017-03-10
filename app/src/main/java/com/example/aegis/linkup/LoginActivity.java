4725d6597b8c1fd88d834d2fce5327257b1405a3
Aegen <kyuzo101@gmail.com>
2017-03-09 23:48:17 -0600

    Added first screen for adding user accounts

--- app/src/main/java/com/example/aegis/linkup/LoginActivity.java
+++ app/src/main/java/com/example/aegis/linkup/LoginActivity.java
@@ -1,13 +1,43 @@
 package com.example.aegis.linkup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button LoginButton;
+    private Button NewUserButton;
+    private EditText UsernameField;
+    private EditText PasswordField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button)findViewById(R.id.loginButton);
+        NewUserButton = (Button)findViewById(R.id.newUserButton);
+        UsernameField = (EditText)findViewById(R.id.loginUsernameField);
+        PasswordField = (EditText)findViewById(R.id.loginPasswordField);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Not Implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        NewUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToNewUser = new Intent(LoginActivity.this, NewUserActivity.class);
+                startActivity(goToNewUser);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/aegis/linkup/NewUserActivity.java
@@ -0,0 +1,38 @@
+package com.example.aegis.linkup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NewUserActivity extends AppCompatActivity {
+
+    private Button NextButton;
+
+    private EditText UsernameField;
+    private EditText PasswordField;
+    private EditText ConfirmPasswordField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+
+
+        NextButton = (Button)findViewById(R.id.newUserNextButton);
+
+        UsernameField = (EditText)findViewById(R.id.newUserUsernameField);
+        PasswordField = (EditText)findViewById(R.id.newUserPasswordField);
+        ConfirmPasswordField = (EditText)findViewById(R.id.newUserConfirmPasswordField);
+
+        NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Not Yet Implemented", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
