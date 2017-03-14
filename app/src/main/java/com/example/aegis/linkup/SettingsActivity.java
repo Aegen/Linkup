a20dfb55f9cae814b9a95688ac0376791390d666
Aegen <kyuzo101@gmail.com>
2017-03-14 13:02:00 -0500

    Michael add stuff

--- app/src/main/java/com/example/aegis/linkup/SettingsActivity.java
+++ app/src/main/java/com/example/aegis/linkup/SettingsActivity.java
@@ -1,15 +1,57 @@
 package com.example.aegis.linkup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    private String Name;
+    private String Location;
+    private String Age;
+    private String Description;
+    private String[] Games;
 
+    private Button ManageProfileButton;
+    private Button ManageGroupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Games = getIntent().getStringArrayExtra("games");
+        Name = getIntent().getStringExtra("name");
+        Age = getIntent().getStringExtra("age");
+        Location = getIntent().getStringExtra("location");
+        Description = getIntent().getStringExtra("description");
+
+        ManageProfileButton = (Button)findViewById(R.id.ManageProfileButton);
+        ManageGroupButton = (Button)findViewById(R.id.ManageGroupProfilesButton);
+
+        ManageProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "It's already perfect", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ManageGroupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent hello = new Intent(SettingsActivity.this, SettingsActivityTwo.class);
+                hello.putExtra("name", Name);
+                hello.putExtra("location", Location);
+                hello.putExtra("age", Age);
+                hello.putExtra("description", Description);
+                hello.putExtra("games", Games);
+                startActivity(hello);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/aegis/linkup/SettingsActivityTwo.java
+++ app/src/main/java/com/example/aegis/linkup/SettingsActivityTwo.java
@@ -1,13 +1,45 @@
 package com.example.aegis.linkup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SettingsActivityTwo extends AppCompatActivity {
+
+    private String Name;
+    private String Location;
+    private String Age;
+    private String Description;
+    private String[] Games;
+
+    private Button YesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_two);
+
+        Games = getIntent().getStringArrayExtra("games");
+        Name = getIntent().getStringExtra("name");
+        Age = getIntent().getStringExtra("age");
+        Location = getIntent().getStringExtra("location");
+        Description = getIntent().getStringExtra("description");
+
+        YesButton = (Button)findViewById(R.id.button7);
+
+        YesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent hello = new Intent(SettingsActivityTwo.this, NewGroupActivity.class);
+                hello.putExtra("name", Name);
+                hello.putExtra("location", Location);
+                hello.putExtra("age", Age);
+                hello.putExtra("description", Description);
+                hello.putExtra("games", Games);
+                startActivity(hello);
+            }
+        });
     }
 }
