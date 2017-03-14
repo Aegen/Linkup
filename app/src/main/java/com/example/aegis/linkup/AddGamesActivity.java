b23fa97f0ad3a33651dd23b2bd1e1416048487b2
Aegen <kyuzo101@gmail.com>
2017-03-13 23:05:24 -0500

    Game choose screen

--- app/src/main/java/com/example/aegis/linkup/AddGamesActivity.java
+++ app/src/main/java/com/example/aegis/linkup/AddGamesActivity.java
@@ -3,6 +3,7 @@ package com.example.aegis.linkup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
@@ -11,6 +12,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class AddGamesActivity extends AppCompatActivity {
@@ -19,6 +21,13 @@ public class AddGamesActivity extends AppCompatActivity {
     private Button AddButton;
     private ListView LView;
     private Button DoneButton;
+
+    private String Name;
+    private String Location;
+    private String Age;
+    private String Description;
+    private ArrayList<String> Games = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +36,11 @@ public class AddGamesActivity extends AppCompatActivity {
         Howard = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
         AddButton = (Button)findViewById(R.id.button2);
         LView = (ListView)findViewById(R.id.listView);
+
+        Name = getIntent().getStringExtra("name");
+        Location = getIntent().getStringExtra("location");
+        Age = getIntent().getStringExtra("age");
+        Description = getIntent().getStringExtra("description");
 
         DoneButton = (Button)findViewById(R.id.button4);
 
@@ -47,6 +61,7 @@ public class AddGamesActivity extends AppCompatActivity {
                     startActivity(a);
 
                     listAdapter.add(Howard.getText().toString());
+                    Games.add(Howard.getText().toString());
                 }
             }
 
@@ -56,6 +71,12 @@ public class AddGamesActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(AddGamesActivity.this, MainProfileActivity.class);
+                i.putExtra("name", Name);
+                i.putExtra("age", Age);
+                i.putExtra("description", Description);
+                i.putExtra("location", Location);
+                Log.d("helo", Games.get(0));
+                i.putExtra("games", Games.toArray(new String[Games.size()]));
                 startActivity(i);
             }
         });
--- app/src/main/java/com/example/aegis/linkup/NewUserActivityTwo.java
+++ app/src/main/java/com/example/aegis/linkup/NewUserActivityTwo.java
@@ -7,22 +7,37 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class NewUserActivityTwo extends AppCompatActivity {
 
 
     private Button NextButton;
+
+    private EditText NameField;
+    private EditText AgeField;
+    private EditText LocationField;
+    private EditText DescriptionField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_user_two);
 
         NextButton = (Button)findViewById(R.id.button);
+        NameField = (EditText)findViewById(R.id.editText);
+        AgeField = (EditText)findViewById(R.id.editText4);
+        DescriptionField = (EditText)findViewById(R.id.editText3);
+        LocationField = (EditText)findViewById(R.id.editText2);
 
         NextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goToGames = new Intent(NewUserActivityTwo.this, AddGamesActivity.class);
+                goToGames.putExtra("name", NameField.getText().toString());
+                goToGames.putExtra("age", AgeField.getText().toString());
+                goToGames.putExtra("description", DescriptionField.getText().toString());
+                goToGames.putExtra("location", LocationField.getText().toString());
                 startActivity(goToGames);
             }
         });
