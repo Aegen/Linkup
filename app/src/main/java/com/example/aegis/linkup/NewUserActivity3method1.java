61903737959321a2a7ab7ab044f1d1948d0d3440
Michael Shanahan <Michael Shanahan>
2017-03-13 21:59:25 -0500

    added victoria's stuff

---
+++ app/src/main/java/com/example/aegis/linkup/NewUserActivity3method1.java
@@ -0,0 +1,22 @@
+package com.example.aegis.linkup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by Victoria on 3/13/17.
+ */
+
+public class NewUserActivity3method1 extends AppCompatActivity  {
+
+    private Button manageProf;
+    private Button manageGroupProf;
+
+
+
+
+
+
+
+
+}
